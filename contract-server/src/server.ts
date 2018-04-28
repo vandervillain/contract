77aedb638f190b6fc551e12efffab992f7c5220b
Aaron Vanderwielen <aaronvander@live.com>
2018-04-28 09:39:19 -0700

    add client

--- contract-server/src/server.ts
+++ contract-server/src/server.ts
@@ -1,8 +1,13 @@
 import Server from 'socket.io';
 import { Store } from 'redux';
+import {Mongoose} from 'mongoose';
 
 export default function startServer(store: Store) {
     const io = new Server().attach(8090);
+
+    Mongoose.connect('mongodb://localhost/1337')
+        .then(() =>  console.log('connection succesful'))
+        .catch((err) => console.error(err));
 
     store.subscribe(() => io.emit('state', store.getState().toJS()));
 
